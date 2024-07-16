4afedc51fe07c136d08c0e2d8457f4bc7400ecf3
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2024-07-16 23:38:14 +0530

    Update validparentheses.cpp

--- day1/validparentheses.cpp
+++ day1/validparentheses.cpp
@@ -1,3 +1,28 @@
-#include<bits/stdc++.h>
- using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+       stack<char> st;
+       for(int i=0;i<s.size();i++)
+       {
+        if(s[i]=='('||s[i]=='{'||s[i]=='[')
+        st.push(s[i]);
+       else {
+                if (!st.empty()) {
+                    
+     if((s[i]==')'&&st.top()=='(')||(s[i]=='}'&&st.top()=='{')||(s[i]==']'&&st.top()=='['))
+        st.pop();
+                
+        else
+        return 0;
+                }
+    
+                else
+                return 0;
+       } 
+       }
+       return st.empty();
+       
+
+    }
+};
 
